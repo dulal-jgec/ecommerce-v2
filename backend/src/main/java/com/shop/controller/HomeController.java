22a77d40c083e2217d32936b6c6496c1b9c793a4
dulal-jgec <skdulalislam1@gmail.com>
2026-07-10 22:56:40 +0530

    home controller add

---
+++ backend/src/main/java/com/shop/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.shop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "ShopLy Backend Running";
+    }
+}
